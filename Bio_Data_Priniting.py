0ba6553c8627b02b946513c42c98c931091fbfbe
Muhammad Awais Ahmad <ak0384426@gmail.com>
2025-09-20 19:39:13 +0500

    Bio_Data_Printing
    A program in which we add our data and its print in a good way

---
+++ Bio_Data_Priniting.py
@@ -0,0 +1,14 @@
+print("Welcome please Enter your Bio Data  Thank You:-   ")
+
+name=input("Enter your name :-")
+F_Name=input("Enter Your Father Name:- ")
+DOB=input("Enter your Date of Birth :-")
+age=int(input("Enter your age:-"))
+CNIC=input("Enter your CNIC :-")
+Qualification=input("Enter your Lates qualification:-")
+Current_Adress=input("Enter Your Currnt Adress Please:-")
+Hobby=input("Enter your Hobby:-")
+
+print("------Your Personal Information------")
+print(f"Your Name: {name}\n,Your F.Name: {F_Name}\nYour Date of Birth: {DOB}\nYour Age: {age}\nYour CNIC:{CNIC}\nYour Lates Qualification:- {Qualification}\nYour Current Adress:-{Current_Adress}\nYour Favorite Hobby:- {Hobby}")
+
